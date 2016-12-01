7fa4d39cba96b22a2c235e47e387f74e35d98301
Ranjan <ranjan.sarma23@gmail.com>
2016-12-01 17:08:41 +0530

    Add comment for randindex and purity

--- utils.py
+++ utils.py
@@ -1,40 +1,41 @@
 from itertools import combinations
 import operator
 
-def calculate_rand_index(original_label_dict, lda_label_dict):
+def calculate_rand_index(original_label_dict, predict_label_dict):
     """
-    @param original_label_dict: dictionary with keys as story_id and value as label
-    @param lda_label_dict: dictionary with keys as story_id and value as label
+    @param original_label_dict: dictionary with keys as document_id and value as label
+    @param predict_label_dict: dictionary with keys as document_id and value as label
     """
     tp = 0
     fp = 0
     fn = 0
     tn = 0
-    for combo in combinations(lda_label_dict.keys(), 2):
+    for combo in combinations(predict_label_dict.keys(), 2):
         id1 = combo[0]
         id2 = combo[1]
-        if lda_label_dict[id1] == lda_label_dict[id2] and original_label_dict[id1] == original_label_dict[id2]:
-            #print (id1,id2),(lda_label_dict[id1],lda_label_dict[id2]), (original_label_dict[id1], original_label_dict[id2])
+        if predict_label_dict[id1] == predict_label_dict[id2] and original_label_dict[id1] == original_label_dict[id2]:
+            #print (id1,id2),(predict_label_dict[id1],predict_label_dict[id2]), (original_label_dict[id1], original_label_dict[id2])
             tp += 1
-        elif lda_label_dict[id1] == lda_label_dict[id2] and original_label_dict[id1] != original_label_dict[id2]:
+        elif predict_label_dict[id1] == predict_label_dict[id2] and original_label_dict[id1] != original_label_dict[id2]:
             fp += 1
-        elif lda_label_dict[id1] != lda_label_dict[id2] and original_label_dict[id1] == original_label_dict[id2]:
+        elif predict_label_dict[id1] != predict_label_dict[id2] and original_label_dict[id1] == original_label_dict[id2]:
             fn += 1
-        elif lda_label_dict[id1] != lda_label_dict[id2] and original_label_dict[id1] != original_label_dict[id2]:
+        elif predict_label_dict[id1] != predict_label_dict[id2] and original_label_dict[id1] != original_label_dict[id2]:
             tn += 1
     rand_index = (tp + tn)/float((tp + tn + fp + fn))
     precision = ( tp * 1.0 ) / (tp + fp)
     recall =  ( tp * 1.0 ) / (tp + fn)
     jc = ( 1.0 * tp ) / ( tp + fp + fn)
     f1 = 2* ((1.0 * precision*recall)/ (precision+recall))
-    purity = calculate_purity(original_label_dict,lda_label_dict)
+    purity = calculate_purity(original_label_dict,predict_label_dict)
     #print 'TP : %d\nTN : %d\nFP : %d\nFN : %d\nRand Index : %f\nPrecision : %f\nRecall : %f\nJC : %f\nPurity : %f\n' % (tp, tn, fp, fn, rand_index, precision, recall, jc, purity)
     string = "%d,%d,%d,%d,%f,%f,%f,%f,%f,%f" % (tp, tn, fp, fn, rand_index, precision, recall, f1, jc, purity)
     return string
 
 def calculate_purity(orig_dict, pred_dict):
     """
- 
+    @param orig_dict: dictionary with keys as document_id and value as label
+    @param pred_dict: dictionary with keys as document_id and value as label
     """
     total = 0
     N = len(pred_dict)
