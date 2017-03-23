7cd54b75ead74f1821729db384a2d0b9d3321f9b
Ranjan Sarmah <ranjan.sarma23@gmail.com>
2017-03-23 10:55:38 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,3 +1,12 @@
+/* Copyright (C) 2017 Ranjan Sarmah - All Rights Reserved
+ * You may use, distribute and modify this code under the
+ * terms of the MIT license, which unfortunately won't be
+ * written for another century.
+ *
+ * You should have received a copy of the MIT license with
+ * this file. If not, please write to: ranjan.sarma23@gmail.com:
+ */
+
 from itertools import combinations
 import operator
 
@@ -5,6 +14,7 @@ def calculate_rand_index(original_label_dict, predict_label_dict):
     """
     @param original_label_dict: dictionary with keys as document_id and value as label
     @param predict_label_dict: dictionary with keys as document_id and value as label
+    @return : string in table format
     """
     tp = 0
     fp = 0
@@ -14,7 +24,6 @@ def calculate_rand_index(original_label_dict, predict_label_dict):
         id1 = combo[0]
         id2 = combo[1]
         if predict_label_dict[id1] == predict_label_dict[id2] and original_label_dict[id1] == original_label_dict[id2]:
-            #print (id1,id2),(predict_label_dict[id1],predict_label_dict[id2]), (original_label_dict[id1], original_label_dict[id2])
             tp += 1
         elif predict_label_dict[id1] == predict_label_dict[id2] and original_label_dict[id1] != original_label_dict[id2]:
             fp += 1
@@ -28,7 +37,6 @@ def calculate_rand_index(original_label_dict, predict_label_dict):
     jc = ( 1.0 * tp ) / ( tp + fp + fn)
     f1 = 2* ((1.0 * precision*recall)/ (precision+recall))
     purity = calculate_purity(original_label_dict,predict_label_dict)
-    #print 'TP : %d\nTN : %d\nFP : %d\nFN : %d\nRand Index : %f\nPrecision : %f\nRecall : %f\nJC : %f\nPurity : %f\n' % (tp, tn, fp, fn, rand_index, precision, recall, jc, purity)
     string = "%d,%d,%d,%d,%f,%f,%f,%f,%f,%f" % (tp, tn, fp, fn, rand_index, precision, recall, f1, jc, purity)
     return string
 
@@ -36,6 +44,7 @@ def calculate_purity(orig_dict, pred_dict):
     """
     @param orig_dict: dictionary with keys as document_id and value as label
     @param pred_dict: dictionary with keys as document_id and value as label
+    @return : purity value
     """
     total = 0
     N = len(pred_dict)
@@ -56,6 +65,4 @@ def calculate_purity(orig_dict, pred_dict):
             else:
                 temp_dict[orig_dict[bid]] = 1
         total += max(temp_dict.iteritems(), key=operator.itemgetter(1))[1]
-        #print total , N
-    #print total , N
     return float(total)/ N
